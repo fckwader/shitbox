5f155a592b4be4929bf2b9488fad1cdf6d2f2317
weddb <weddbrc@gmail.com>
2024-05-25 16:47:33 +0200

    YEAH

--- mm.cu
+++ mm.cu
@@ -11,7 +11,7 @@ void init(float **a, float **b, float **c, int n){
     }
 }
 
-void mm(float **a, float **b, float **c, int n){
+void mm(float *a[], float *b[], float *c[], int n){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             for(int k = 0; k < n; k++){
