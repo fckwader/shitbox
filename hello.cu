37cc47837aeca0a3604f47b9b2bbcafcb18ffed4
weddb <weddbrc@gmail.com>
2024-05-25 15:15:22 +0200

    YEAH

--- hello.cu
+++ hello.cu
@@ -52,7 +52,7 @@ void runBench(int n, float *x, float *y, float *z, int bx, int by, int bz, int d
 
 int main()
 {
-    int n = 1000000;
+    int n = 100000000;
     float *x, *y, *z;
     cudaMallocManaged(&x, n*sizeof(float));
     cudaMallocManaged(&y, n*sizeof(float));
