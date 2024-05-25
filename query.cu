a75598e19f4c8c02261f283a678ddf3df157f6ae
weddb <weddbrc@gmail.com>
2024-05-25 15:47:31 +0200

    YEAH

--- query.cu
+++ query.cu
@@ -10,6 +10,11 @@ int main() {
   for (int i = 0; i < nDevices; i++) {
     cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, i);
+    printf("Device name: %s\n", prop.name);
     printf("Multiprocessor count: %d\n", prop.multiProcessorCount);
+    printf("Max Blocks per Multiprocessor: %d\n", prop.maxBlocksPerMultiProcessor);
+    printf("Max Threads per Block: %d\n", prop.maxThreadsPerBlock);
+    printf("Max threads per MP: %d\n", prop.maxThreadsPerMultiProcessor);
+    printf("\n");
   }
 }
