6d74e0cc6053bf92d177b47e5c96ba965f360c41
weddb <weddbrc@gmail.com>
2024-05-25 16:49:42 +0200

    YEAH

--- mm.cu
+++ mm.cu
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 
-void init(float **a, float **b, float **c, int n){
+void init(float *a, float *b, float *c, int n){
     for(int i = 0; i < n; i++){
         for(int j = 0; j<n; j++){
-            a[i][j] = 0;
-            b[i][j] = (i * j) % 16;
-            c[i][j] = (i*j + 1)% 13;
+            a[i*n + j] = 0;
+            b[i*n + j] = (i * j) % 16;
+            c[i*n + j] = (i*j + 1)% 13;
         }
     }
 }
 
-void mm(float *a[], float *b[], float *c[], int n){
+
+
+void mm(float *a, float *b, float *c, int n){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             for(int k = 0; k < n; k++){
-                a[i][j] += b[i][k] * c[k][j];
+                a[i*n +j] += b[i*n + k] * c[k*n + j];
             }
         }
     }
@@ -23,11 +25,12 @@ void mm(float *a[], float *b[], float *c[], int n){
 
 int main(){
     int n = 32;
-    float a[n][n], b[n][n], c[n][n];
+    float a[n*n], b[n*n], c[n*n];
 
     init(a, b, c, n);
     mm(a, b, c, n);
     return 0;
+
 }
 
 
