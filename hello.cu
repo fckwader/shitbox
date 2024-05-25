45d503f8ea1f8713aecbadb12ac294cf7c4f304f
weddb <weddbrc@gmail.com>
2024-05-25 12:22:45 +0200

    YEAH

--- hello.cu
+++ hello.cu
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-int n = 1000000;
-float x[n];
-float y[n];
 
-//init
-for(int i = 0; i < n; i++){
-    x[i] = (i * 2) % 13;
-    y[i] = (i * 3) % 25;
-}
+
+
 
 void add(int n, float *x, float *y)
 {
@@ -19,6 +13,16 @@ void add(int n, float *x, float *y)
 
 int main()
 {
+    int n = 1000000;
+    float x[n];
+    float y[n];
+
+    //init
+    for(int i = 0; i < n; i++){
+        x[i] = (i * 2) % 13;
+        y[i] = (i * 3) % 25;
+    }
+
     add(n, x, y);
     return 0;
 }
