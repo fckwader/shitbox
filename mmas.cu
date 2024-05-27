142720793b403ea83c15fce16bdfb8eb7cbcbbd0
weddb <weddbrc@gmail.com>
2024-05-27 14:43:02 +0200

    YEAH

--- mmas.cu
+++ mmas.cu
@@ -18,6 +18,7 @@ __global__ void globalMM(double *__restrict__ a,
 
     for (int r = 0; r < REP; ++r)
         if (col < N && row < N) {
+            #pragma unroll
             for (int i = 0; i < N; i++) {
                 sum += a[row * N + i] * b[i * N + col];
                 //flopcount += 2;
