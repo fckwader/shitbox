55bca39732dc27597f736364ad709128f45f9954
ge96zip <weddbrc@gmail.com>
2024-06-19 21:31:28 +0200

    YEAH

--- proj1/Jacobi.h
+++ proj1/Jacobi.h
@@ -58,7 +58,7 @@ public:
                     printf("%d ", pos);
                     pos++;
                     if(tx == _nx){
-                        pos+=1;
+                        pos+=2;
                     }
                 }
 
