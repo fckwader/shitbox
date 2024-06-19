21f29347c125ed5a7de74cb080fea335cd991d1e
ge96zip <weddbrc@gmail.com>
2024-06-19 18:34:43 +0200

    YEAH

--- proj1/horse.cpp
+++ proj1/horse.cpp
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
     int n = 32768;
 
-    //regular
     double *m = (double *) malloc(n * n * sizeof(double));
 
     for(int i = 0; i < n * n; i++){
         m[i] = (i+5) % 13;
     }
 
-
-
+    //regular style
+    clock_t start = clock();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            m[i*n + j] += 15;
+        }
+    }
+    clock_t end = clock();
+    float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    printf("Regular style took %.2f seconds\n", seconds);
 
 
 
