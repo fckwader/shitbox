0230b6fe06a8ae058408e0a5f341847385a786c0
ge96zip <weddbrc@gmail.com>
2024-06-19 18:38:08 +0200

    YEAH

--- proj1/horse.cpp
+++ proj1/horse.cpp
@@ -23,6 +23,25 @@ int main()
     float seconds = (float)(end - start) / CLOCKS_PER_SEC;
     printf("Regular style took %.2f seconds\n", seconds);
 
+    int tilesize = 8;
+
+    start = clock();
+    for(int i = 0; i < n; i += tilesize){
+        for(int j = 0; j < n; j += tilesize){
+
+            for(int ti = i; ti < i + tilesize && ti < n; ti++){
+                for(int tj = j; tj < j + tilesize && tj< n; tj++){
+                        m[ti*n + tj += 15];
+                }
+            }
+
+
+        }
+    }
+    end = clock();
+    seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    printf("Tiled style took %.2f seconds\n", seconds);
+
 
 
     return 0;
