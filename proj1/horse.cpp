cfc9dc91cccac7e0bad44d8f9e021a8ad659c336
ge96zip <weddbrc@gmail.com>
2024-06-19 18:30:27 +0200

    YEAH

--- proj1/horse.cpp
+++ proj1/horse.cpp
@@ -6,7 +6,7 @@ int main()
     int n = 32768;
 
     //regular
-    double *m = malloc(n * n * sizeof(souble));
+    double *m = malloc(n * n * sizeof(double));
 
     for(int i = 0; i < n * n; i++){
         m[i] = (i+5) % 13;
