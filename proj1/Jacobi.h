e95efa4f356784432696cd8828d99f304fdc807e
weddb <weddbrc@gmail.com>
2024-06-20 17:01:26 +0200

    YEAH

--- proj1/Jacobi.h
+++ proj1/Jacobi.h
@@ -45,11 +45,11 @@ public:
 
     unsigned int ts = 32;
     unsigned int pos = 0;
-    #pragma omp parallel for collapse(2)
     for (unsigned int y = 0; y < _ny; y++)
     {
       for (unsigned int x = 0; x < _nx; x+=ts)
       {
+        #pragma omp unroll
         for(int tx = x; tx < x + ts && tx < _nx; tx++){
                 // do Jacobi update and write to writePtr
                 //printf("X%d Y%d P%d\n", tx, y, pos);
