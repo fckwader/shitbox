a5d3033b816f3386de1ed09deb375189990ced55
ge96zip <weddbrc@gmail.com>
2024-06-20 13:56:26 +0200

    YEAH

--- proj1/Jacobi.h
+++ proj1/Jacobi.h
@@ -42,46 +42,24 @@ public:
     FLOAT *writePtr = writeField + (_nx + 3);
 
     // use pos to advance access through the whole grid without any expensive index computations
-    unsigned int tilesize = 4;
     unsigned int pos = 0;
-    #pragma omp parallel for collapse(2)
-    for (unsigned int y = 1; y < _ny + 1; y+=tilesize)
+    for (unsigned int y = 1; y < _ny + 1; y++)
     {
-        for (unsigned int x = 1; x < _nx + 1; x+=tilesize)
-        {
-            int count = 0;
-            for(unsigned int ty = y; ty < y + tilesize && ty < _ny + 1; ty++){
-                for(unsigned int tx = x; tx < x + tilesize && tx < _nx + 1; tx++){
-                    writePtr[pos] = _RHS * rhsPtr[pos];
-                    writePtr[pos] += _X * (readPtr_W[pos] + readPtr_E[pos]);
-                    writePtr[pos] += _Y * (readPtr_S[pos] + readPtr_N[pos]);
-                    //printf("%d ", pos);
-                    pos++;
-                    if(tx == _nx){
-                        pos+=1;
-                    }
-                }
+      for (unsigned int x = 1; x < _nx + 1; x++)
+      {
+        // do Jacobi update and write to writePtr
+        writePtr[pos] = _RHS * rhsPtr[pos];
+        writePtr[pos] += _X * (readPtr_W[pos] + readPtr_E[pos]);
+        writePtr[pos] += _Y * (readPtr_S[pos] + readPtr_N[pos]);
 
-                //printf("\n");
-                pos += _ny + 2;
-                pos -= tilesize;
-                count++;
-
-            }
-            //printf("Count: %d\n", count);
-            //printf("Old: %d ", pos);
-
-            pos += tilesize;
-            //printf("Mid: %d ", pos);
-            pos -= (count) * (_ny + 2);
-            count = 0;
-            //printf("new: %d\n", pos);
+        // update pos along x-axis
+        pos++;
       }
-      //printf("\n full old: %d ", pos);
-      pos += (tilesize - 1) * (_ny + 2) + 1;
-      //printf("new: %d\n", pos);
+
+      // update pos along y-axis; therefore just jump over the two boundary values
+      pos += 2;
     }
-    }
+  }
 
 private:
   // returns the prefactor for the Jacobi stencil in x-direction
